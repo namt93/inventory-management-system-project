3001868f53a1ecbc0cde86364b1a57b7bbb1a34b
namt93 <namt7115633@gmail.com>
2023-06-04 21:00:11 +0700

    Create trademark and search for header

--- front-end/inventory-ui/src/components/Layout/components/Header/index.js
+++ front-end/inventory-ui/src/components/Layout/components/Header/index.js
@@ -1,13 +1,50 @@
 import styles from "./Header.module.scss";
 import classNames from "classnames/bind";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faComputer } from "@fortawesome/free-solid-svg-icons";
+
 const cx = classNames.bind(styles);
 function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        {/* Logo */}
-        {/* Search */}
+        <div className={"row"}>
+          {/* Logo */}
+          <div className={"col-sm-3"}>
+            <a className={cx("header-trademark")} href="/">
+              <FontAwesomeIcon icon={faComputer} /> iPAC Lab
+            </a>
+          </div>
+          {/* Search */}
+          <div className="col-sm-6">
+            <div className={cx("header-search")}>
+              <input
+                className={cx("input-search")}
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                spellCheck={false}
+              />
+              <button className={cx("btn-search")} type="submit">
+                Search
+              </button>
+            </div>
+          </div>
+          <div className={"col-sm-2"}>
+            <div className={cx("header-notification")}></div>
+          </div>
+          {/* Header more */}
+          <div className="col-sm-1">
+            <div className={cx("header-more")}>
+              <div className={cx("header-more-title")}>namt93</div>
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className={cx("fa-chevron-down")}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
--- front-end/inventory-ui/src/index.js
+++ front-end/inventory-ui/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "~/App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyles from "./components/GlobalStyles";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
