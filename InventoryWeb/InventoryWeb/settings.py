46bda7cf953cc4ae7098d1b5c8c9538ea8b29f90
namt93 <namt7115633@gmail.com>
2023-06-14 15:11:41 +0700

    Solve CORS and install axios

--- InventoryWeb/InventoryWeb/settings.py
+++ InventoryWeb/InventoryWeb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'racks',
     'rest_framework',
     'oauth2_provider',
@@ -57,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'InventoryWeb.urls'
@@ -137,3 +140,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3002",
+    "http://127.0.0.1:9000",
+]
