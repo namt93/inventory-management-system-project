e7dc7f62480ee5fe7ad4eaf0be7a690347dda142
namt93 <namt7115633@gmail.com>
2023-07-21 02:27:14 +0700

    Create add-rack, add-document, documents/search

--- front-end/inventory-ui/src/routes/index.js
+++ front-end/inventory-ui/src/routes/index.js
@@ -4,10 +4,14 @@ import Document from "~/pages/Document";
 import Login from "~/pages/Login";
 import Setting from "~/pages/Setting";
 import RackStatus from "~/pages/RackStatus";
+import DocumentAdd from "~/pages/PageAdd/DocumentAdd";
+import RackAdd from "~/pages/PageAdd/RackAdd";
 
 const publicRoutes = [
   { path: "/", component: Rack },
   { path: "/racks/rack/:id", component: RackStatus },
+  { path: "/add-document", component: DocumentAdd },
+  { path: "/add-rack", component: RackAdd },
   { path: "/documents", component: Document },
   { path: "/documents/search?query=:query", component: Document },
   { path: "/documents/:id", component: Document },
